4441dabb425b15fcddc5a516772fafa5f599d899
CLEARMATCH\john.geng <john.geng@clearmatch.com>
2022-06-13 11:38:18 +1200

    Setup API

---
+++ Application/API/Controllers/FormController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FormController : ControllerBase
+    {
+        [HttpGet()]
+        public async Task<ActionResult> Index()
+        {
+            return new OkResult();
+        }
+    }
+}
